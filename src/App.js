e7797a1bb0c957a2b1b48b8f030e488525f10140
Joseph Isbell <shadowwolf347@comcast.net>
2018-09-23 13:03:54 -0600

    Added onChange event handler and two-way binding

--- src/App.js
+++ src/App.js
@@ -22,6 +22,14 @@ class App extends Component {
 			]})
 	};
 
+	nameChangeHandler = (event) => {
+		this.setState({persons: [
+				{name: 'Manu', age: 28 },
+				{name: event.target.value, age: 30},
+				{name: 'Rachel', age: 32}
+			]})
+	};
+
 	render() {
 		return (
 			<div className="App">
@@ -34,7 +42,9 @@ class App extends Component {
 				<Person
 					name={this.state.persons[1].name}
 					age={this.state.persons[1].age}
-					click={this.switchNameHandler.bind(this, 'Joe')}>My Hobbies: Sewing</Person>
+					click={this.switchNameHandler.bind(this, 'Joe')}
+					changed={this.nameChangeHandler}>My Hobbies: Sewing</Person>
+
 				<Person
 					name={this.state.persons[2].name}
 					age={this.state.persons[2].age}/>
