ecc8168a14add48ec2dbbe93834a23f8510fb377
Joseph Isbell <shadowwolf347@comcast.net>
2018-09-23 09:56:22 -0600

    Duplicated the Person tag twice to show component reusability.

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ class App extends Component {
          <h1>Hi, Im a React App</h1>
          <p>This is really working!</p>
          <Person />
+			<Person />
+			<Person />
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, "Does this work now?"));
