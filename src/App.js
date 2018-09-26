87f40c302aec97ca8fc8d25001c79f2824782ff0
Joseph Isbell <shadowwolf347@comcast.net>
2018-09-26 16:18:37 -0600

    Added hover pseudo selector and StyleRoot component to enable media query useage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import Radium from 'radium';
+import Radium, {StyleRoot} from 'radium';
 import './App.css';
 import Person from './Person/Person';
 
@@ -51,7 +51,11 @@ class App extends Component {
 			font: 'inherit',
 			border: '1px solid blue',
 			padding: '8px',
-			cursor: 'pointer'
+			cursor: 'pointer',
+			':hover': {
+				backgroundColor: 'lightgreen',
+				color: 'black'
+			}
 		};
 
 		let persons = null;
@@ -71,6 +75,10 @@ class App extends Component {
 				</div>
 			);
 			style.backgroundColor = 'red';
+			style[':hover'] = {
+				backgroundColor: 'salmon',
+					color: 'black'
+			}
 		}
 
 		const classes = [];
@@ -83,15 +91,17 @@ class App extends Component {
 		}
 
 		return (
-			<div className="App">
-				<h1>Hi, Im a React App</h1>
-				<p className={classes.join(' ')}>This is really working!</p>
-				<button
-					style={style}
-					onClick={this.togglePersonsHandler}>Toggle Persons
-				</button>
-				{persons}
-			</div>
+			<StyleRoot>
+				<div className="App">
+					<h1>Hi, Im a React App</h1>
+					<p className={classes.join(' ')}>This is really working!</p>
+					<button
+						style={style}
+						onClick={this.togglePersonsHandler}>Toggle Persons
+					</button>
+					{persons}
+				</div>
+			</StyleRoot>
 		);
 		// return React.createElement('div', {className: 'App'}, React.createElement('h1', null, "Does this work now?"));
 	}
