d8692dcd2a1ad5570a76178887c7e1a7c31d173a
Joseph Isbell <shadowwolf347@comcast.net>
2018-09-26 16:29:13 -0600

    Removed Radium from files in order to use CSS modules

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import Radium, {StyleRoot} from 'radium';
 import './App.css';
 import Person from './Person/Person';
 
@@ -51,11 +50,7 @@ class App extends Component {
 			font: 'inherit',
 			border: '1px solid blue',
 			padding: '8px',
-			cursor: 'pointer',
-			':hover': {
-				backgroundColor: 'lightgreen',
-				color: 'black'
-			}
+			cursor: 'pointer'
 		};
 
 		let persons = null;
@@ -75,10 +70,6 @@ class App extends Component {
 				</div>
 			);
 			style.backgroundColor = 'red';
-			style[':hover'] = {
-				backgroundColor: 'salmon',
-					color: 'black'
-			}
 		}
 
 		const classes = [];
@@ -91,7 +82,6 @@ class App extends Component {
 		}
 
 		return (
-			<StyleRoot>
 				<div className="App">
 					<h1>Hi, Im a React App</h1>
 					<p className={classes.join(' ')}>This is really working!</p>
@@ -101,10 +91,10 @@ class App extends Component {
 					</button>
 					{persons}
 				</div>
-			</StyleRoot>
+
 		);
 		// return React.createElement('div', {className: 'App'}, React.createElement('h1', null, "Does this work now?"));
 	}
 }
 
-export default Radium(App);
+export default App;
