9eade7c4e5e1479b3e46ce6e4b30cf4a14d8ebf1
Joseph Isbell <shadowwolf347@comcast.net>
2018-09-26 16:56:59 -0600

    Removed inline styling. Added btn class variable to the button element

--- src/App.js
+++ src/App.js
@@ -44,16 +44,8 @@ class App extends Component {
 	};
 
 	render() {
-		const style = {
-			backgroundColor: 'green',
-			color: 'white',
-			font: 'inherit',
-			border: '1px solid blue',
-			padding: '8px',
-			cursor: 'pointer'
-		};
-
 		let persons = null;
+		let btnClass = '';
 
 		if (this.state.showPersons) {
 			persons = (
@@ -69,7 +61,8 @@ class App extends Component {
 					})}
 				</div>
 			);
-			style.backgroundColor = 'red';
+
+			btnClass = classes.Red;
 		}
 
 		const assignedClasses = [];
@@ -86,7 +79,7 @@ class App extends Component {
 					<h1>Hi, Im a React App</h1>
 					<p className={assignedClasses.join(' ')}>This is really working!</p>
 					<button
-						style={style}
+						className={btnClass}
 						onClick={this.togglePersonsHandler}>Toggle Persons
 					</button>
 					{persons}
