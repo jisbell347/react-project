2502b68dc62762d61346a815f5f8d13ac3581a70
Joseph Isbell <shadowwolf347@comcast.net>
2018-09-26 20:54:42 -0600

    Restructured directories for better readability and flow

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import classes from './App.css';
-import Person from './Person/Person';
-import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
+import Persons from '../Components/Persons/Persons';
 
 class App extends Component {
 	state = {
@@ -51,15 +50,10 @@ class App extends Component {
 		if (this.state.showPersons) {
 			persons = (
 				<div>
-					{this.state.persons.map((person, index) => {
-						return <ErrorBoundary><Person
-							click={() => this.deletePersonHandler(index)}
-							name={person.name}
-							age={person.age}
-							key={person.id}
-							changed={(event) => this.nameChangeHandler(event, person.id)}
-						/></ErrorBoundary>
-					})}
+					<Persons
+						persons={this.state.persons}
+					clicked={this.deletePersonHandler}
+					changed={this.nameChangeHandler}/>
 				</div>
 			);
 
@@ -69,10 +63,10 @@ class App extends Component {
 		const assignedClasses = [];
 
 		if(this.state.persons.length <=2) {
-			classes.push(classes.red); //classes = ['red']
+			assignedClasses.push(classes.red); //classes = ['red']
 		}
 		if(this.state.persons.length <=1) {
-			classes.push(classes.bold); //classes = ['red', 'bold']
+			assignedClasses.push(classes.bold); //classes = ['red', 'bold']
 		}
 
 		return (
