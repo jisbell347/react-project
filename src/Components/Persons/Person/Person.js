45a84d09b37706626cb955f7eae91a7433e38b9a
Joseph Isbell <shadowwolf347@comcast.net>
2018-09-28 16:32:43 -0600

    Refactored the withClass component

--- src/Components/Persons/Person/Person.js
+++ src/Components/Persons/Person/Person.js
@@ -8,22 +8,28 @@ class Person extends Component {
 	constructor(props) {
 		super(props);
 		console.log('[Person.js] Inside Constructor', props);
+		this.inputElement = React.createRef();
 	}
 
 	componentDidMount() {
 		console.log('[Person.js] Inside componentDidMount()');
 		if(this.props.position === 0) {
-			this.inputElement.focus();
+			this.inputElement.current.focus();
 		}
+	}
+
+	focus() {
+		this.inputElement.current.focus();
 	}
 
 	render(){
 		return (
-			<Auxiliary classes={classes.Person}>
+			<Auxiliary>
+				{this.props.authenticated ? <p>I'm authenticated!</p> : null}
 				<p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
 				<p>{this.props.children}</p>
 				<input
-						ref={(inp) =>{this.inputElement = inp}}
+						ref={this.inputElement}
 						type="text"
 						onChange={this.props.changed}
 						value={this.props.name}/>
--- src/Components/Persons/Persons.js
+++ src/Components/Persons/Persons.js
@@ -1,15 +1,37 @@
-import React, {Component} from 'react';
+import React, {PureComponent} from 'react';
 import ErrorBoundary from "../ErrorBoundary/ErrorBoundary";
 import Person from "./Person/Person";
 
-class Persons extends Component {
+class Persons extends PureComponent {
+	constructor(props) {
+		super(props);
+		console.log('[Persons.js] inside Constructor', props);
+		this.lastPersonRef = React.createRef();
+	}
+
+	componentWillMount(){
+		console.log('[Persons.js] inside componentWillMount()');
+	}
+
+	componentDidMount() {
+		console.log('[Persons.js] inside componentDidMount()');
+		this.lastPersonRef.current.focus();
+	}
+
+	componentWillRecieveProps(nextProps) {
+		console.log('[Update Persons.js] inside componentWillRecieveProps()', nextProps);
+	}
+
 	render() {
+		console.log('[Persons.js] inside render()');
 		return this.props.persons.map((person, index) => {
 				return <ErrorBoundary><Person
 					click={() => this.props.clicked(index)}
 					name={person.name}
 					position={index}
 					age={person.age}
+					ref={this.lastPersonRef}
+					authenticated={this.props.isAuthenticated}
 					key={person.id}
 					changed={(event) => this.props.changed(event, person.id)}
 				/></ErrorBoundary>
